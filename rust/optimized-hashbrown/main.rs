0ae2033e1fc5532f61e2b08678e995fb499e4750
Andrew Gallant <jamslam@gmail.com>
2022-07-25 20:29:28 -0400

    add 'optimized-hashbrown' variant
    This adds the 'hashbrown' method used in 'kimono-simple' to test how
    much of a difference it specifically makes. This doesn't seem to change
    runtime performance at all.

---
+++ rust/optimized-hashbrown/main.rs
@@ -0,0 +1,81 @@
+// This version is an approximate port of the optimized Go program. Its buffer
+// handling is slightly simpler: we don't bother with dealing with the last
+// newline character. (This may appear to save work, but it only saves work
+// once per 64KB buffer, so is likely negligible. It's just simpler IMO.)
+//
+// There's nothing particularly interesting here other than swapping out std's
+// default hashing algorithm for one that isn't cryptographically secure.
+
+use std::{
+    error::Error,
+    io::{self, Read, Write},
+};
+
+use hashbrown::HashMap;
+
+fn main() {
+    if let Err(err) = try_main() {
+        eprintln!("{}", err);
+        std::process::exit(1);
+    }
+}
+
+fn try_main() -> Result<(), Box<dyn Error>> {
+    let stdin = io::stdin();
+    let mut stdin = stdin.lock();
+    let mut counts: HashMap<Box<[u8]>, u64> = HashMap::default();
+    let mut buf = vec![0; 64 * (1 << 10)];
+    let mut offset = 0;
+    let mut start = None;
+    loop {
+        let nread = stdin.read(&mut buf[offset..])?;
+        if nread == 0 {
+            if offset > 0 {
+                increment(&mut counts, &buf[..offset + 1]);
+            }
+            break;
+        }
+        let buf = &mut buf[..offset + nread];
+
+        for i in (0..buf.len()).skip(offset) {
+            let b = buf[i];
+            if b'A' <= b && b <= b'Z' {
+                buf[i] += b'a' - b'A';
+            }
+            if b == b' ' || b == b'\n' {
+                if let Some(start) = start.take() {
+                    increment(&mut counts, &buf[start..i]);
+                }
+            } else if start.is_none() {
+                start = Some(i);
+            }
+        }
+        if let Some(ref mut start) = start {
+            offset = buf.len() - *start;
+            buf.copy_within(*start.., 0);
+            *start = 0;
+        } else {
+            offset = 0;
+        }
+    }
+
+    let mut ordered: Vec<_> = counts.into_iter().collect();
+    ordered.sort_unstable_by_key(|&(_, count)| count);
+
+    for (word, count) in ordered.into_iter().rev() {
+        writeln!(io::stdout(), "{} {}", std::str::from_utf8(&word)?, count)?;
+    }
+    Ok(())
+}
+
+fn increment(counts: &mut HashMap<Box<[u8]>, u64>, word: &[u8]) {
+    // using 'counts.entry' would be more idiomatic here, but doing so requires
+    // allocating a new Vec<u8> because of its API. Instead, we do two hash
+    // lookups, but in the exceptionally common case (we see a word we've
+    // already seen), we only do one and without any allocs.
+    if let Some(count) = counts.get_mut(word) {
+        *count += 1;
+        return;
+    }
+    counts.insert_unique_unchecked(Box::from(word), 1);
+}
